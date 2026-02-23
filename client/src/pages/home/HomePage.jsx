20db0a4fb992c5cc0f48bc67d7f8089412c9a21c
DevMo.21x <DevMo.21x@outlook.com>
2026-02-23 14:18:39 -0400

    feat: Update API endpoints in HomePage and configure Vite server proxy

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import CheckmarkIcon from "../../assets/images/icons/checkmark.png";
 import HomeFavIcon from "../../assets/images/home-favicon.png";
-// import { products } from "../../../starting-code/data/products";
 import "../../index.css";
 import "./HomePage.css";
 
@@ -12,11 +11,11 @@ export const HomePage = () => {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products").then((response) => {
+    axios.get("/api/products").then((response) => {
       setProducts(response.data);
     });
 
-    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+    axios.get("/api/cart-items").then((response) => {
       setCart(response.data);
     });
   }, []);
